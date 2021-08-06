958cb41099d95519fd2e579c3b34704e0303d0a4
ErikDMCosta <53874072+ErikDMCosta@users.noreply.github.com>
2021-08-06 20:41:08 -0300

    Add arquivos das aulas do curso PHP POO via upload

---
+++ Aula15b/index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <pre>
+            <?php
+                require_once 'Video.php';
+                $v[0] = new Video("Aula 1 de POO");
+                $v[1] = new Video("Aula 12 de PHP");
+                $v[2] = new Video("Aula 15 de HTML5");
+                
+                require_once 'Gafanhoto.php';
+                $g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
+                $g[1] = new Gafanhoto("Creuza", 12, "F", "creuzita");
+                
+                require_once 'Visualizacao.php';
+                $vis[0] = new Visualizacao($g[0], $v[2]); // Jubileu assiste HTML5
+                $vis[1] = new Visualizacao($g[0], $v[1]); // Jubileu assiste PHP
+                
+                $vis[0]->avaliar();
+                $vis[1]->avaliarPorc(85);
+                
+                print_r($vis);
+                
+            ?>
+        </pre>
+    </body>
+</html>
